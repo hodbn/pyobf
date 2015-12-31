87090cbdb07ffdd132cf161d4864adaad64b5259
17Batata <17Batata@gmail.com>
2015-12-31 22:56:36 +0200

    added js majority chooser

--- pyobf/choosers.py
+++ pyobf/choosers.py
@@ -1,16 +1,39 @@
-class BaseChooser(object):
-    def __init__(self, lang):
-        self.lang = lang
+import jinja2
+from languages import *
 
+
+tpl_loader = jinja2.FileSystemLoader(searchpath='templates')
+tpl_env = jinja2.Environment(loader=tpl_loader)
+
+
+class BaseChooser(object):
     def choose(self, progs):
         raise NotImplementedError()
+
+
+class JSChooserMixin:
+    @property
+    def lang(self):
+        return LANG_JS
+
+
+class CChooserMixin:
+    @property
+    def lang(self):
+        return LANG_C
 
 
 class MajorityChooser(BaseChooser):
+    pass
+
+
+class JSMajorityChooser(MajorityChooser, JSChooserMixin):
+    _TPL_FN = r'js-majority-chooser.tpl'
+
     def choose(self, progs):
-        raise NotImplementedError()
+        tpl = tpl_env.get_template(self._TPL_FN)
+        return tpl.render(progs=progs)
 
 
 class DefensiveChooser(BaseChooser):
-    def choose(self, progs):
-        raise NotImplementedError()
+    pass
--- pyobf/main.py
+++ pyobf/main.py
@@ -1,5 +1,6 @@
 import sys
-from combiners import CascadeCombiner
+from choosers import JSMajorityChooser
+from combiners import CascadeCombiner, C4OutOf3Combiner
 from obfuscators import YUIObfuscator
 
 
@@ -9,12 +10,13 @@ JQUERY_PATH = r'..\obfuscators\js\yuicompressor-2.4.8-leak\tests\jquery-1.6.4.js
 
 def main():
     o = YUIObfuscator(YUI_PATH, randomize=True)
-    c = CascadeCombiner([o, o, o, o])
+    m = JSMajorityChooser()
+    cascade = C4OutOf3Combiner(obfs=[o, o, o, o], maj=m)
     with open(JQUERY_PATH, 'rb') as f:
         prog = f.read()
 
     print len(o.obfuscate(prog))
-    print len(c.combine(prog))
+    print len(cascade.combine(prog))
 
     return 0
 
