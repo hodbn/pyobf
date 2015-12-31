5c2a57ef72a7775d89ab973600887c9c1793c9da
Hod Bin Noon <bin.noon.hod@gmail.com>
2016-01-01 00:25:54 +0200

    added closure obfuscator support

--- pyobf/obfuscators.py
+++ pyobf/obfuscators.py
@@ -24,6 +24,26 @@ class CObfuscatorMixin:
         return LANG_C
 
 
+def _get_jar_input_output(jar, prog, args):
+    env = os.environ
+    in_fn, out_fn = 'in.js', 'out.js'
+    env['IN_FILE'], env['OUT_FILE'] = in_fn, out_fn
+    try:
+        with open(in_fn, 'wb') as infile:
+            infile.write(prog)
+        try:
+            p = subprocess.Popen(['java', '-jar', jar] + list(args),
+                                 shell=True, env=env)
+            p.wait()
+            assert p.returncode == 0
+            with open(out_fn, 'rb') as outfile:
+                return outfile.read()
+        finally:
+            os.unlink(out_fn)
+    finally:
+        os.unlink(in_fn)
+
+
 class YUIObfuscator(BaseObfuscator, JSObfuscatorMixin):
     _VALID_LEAKS_MAP = {
         'in-code': 'output',
@@ -41,30 +61,22 @@ class YUIObfuscator(BaseObfuscator, JSObfuscatorMixin):
             assert self.leak in self._VALID_LEAKS_MAP
             self.leak = self._VALID_LEAKS_MAP[leak]
 
-    def _run_jar(self, *args):
-        p = subprocess.Popen(['java', '-jar', self.jar] + list(args),
-                             shell=True)
-        p.wait()
-        return p.returncode == 0
+    def obfuscate(self, prog):
+        args = []
+        if self.randomize:
+            args.append('--randomize')
+        if self.leak:
+            args.append('--leaktype=%s' % (self.leak, ))
+        args.extend(['-o', '%OUT_FILE%', '%IN_FILE%'])
+        return _get_jar_input_output(self.jar, prog, args)
+
+
+class ClosureObfuscator(BaseObfuscator, JSObfuscatorMixin):
+    def __init__(self, jar):
+        super(ClosureObfuscator, self).__init__()
+        self.jar = jar
 
     def obfuscate(self, prog):
-        in_fn = 'in.js'
-        out_fn = 'out.js'
-        try:
-            with open(in_fn, 'wb') as infile:
-                infile.write(prog)
-            try:
-                args = []
-                if self.randomize:
-                    args.append('--randomize')
-                if self.leak:
-                    args.append('--leaktype=%s' % (self.leak, ))
-                args.extend(['-o', out_fn, infile.name])
-                if not self._run_jar(*args):
-                    return None
-                with open(out_fn, 'rb') as outfile:
-                    return outfile.read()
-            finally:
-                os.unlink(out_fn)
-        finally:
-            os.unlink(in_fn)
+        args = ['-O', 'SIMPLE_OPTIMIZATIONS', '--js', '%IN_FILE%',
+                '--js_output_file', '%OUT_FILE%']
+        return _get_jar_input_output(self.jar, prog, args)
