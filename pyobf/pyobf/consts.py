1a720f561359539a0b4efb15792a35bba70c34ff
Hod Bin Noon <bin.noon.hod@gmail.com>
2016-01-02 18:34:49 +0200

    added angularjs

--- pyobf/pyobf/consts.py
+++ pyobf/pyobf/consts.py
@@ -11,6 +11,7 @@ CLOSURE_PATH = os.path.join(_js_obf, r'closure\compiler.jar')
 JS_FIB = os.path.join(_tests, r'fib.js')
 JS_FACT = os.path.join(_tests, r'fact.js')
 JS_JQUERY = os.path.join(_js_obf, r'yuicompressor-2.4.8-leak\tests\jquery-1.6.4.js')
+JS_ANGULAR = os.path.join(_tests, r'angular-1.4.5.js')
 
 
 del _base
