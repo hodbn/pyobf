d5b747d8b479c4b307466ef297fd4baab115e988
Hod Bin Noon <bin.noon.hod@gmail.com>
2016-01-08 14:37:09 +0200

    added bunch of js libraries for testing

--- pyobf/main.py
+++ pyobf/main.py
@@ -9,55 +9,98 @@ from pyobf.obfuscators import YUIObfuscator, ClosureObfuscator
 from pyobf.program import Program
 
 
+REPEAT = 10
+
+
+def _avg(l):
+    assert len(l) > 0
+    return float(sum(l)) / len(l)
+
+
 def profile_obfuscator(o, p):
-    print '%s:' % (o, ),
-    s = time.time()
-    out = o.obfuscate(p)
-    e = time.time()
-    print 'size=%d,time=%3.3f' % (len(out.code), e - s)
-profile_combiner = profile_obfuscator
+    times = []
+    lens = []
+    for _ in xrange(REPEAT):
+        s = time.time()
+        out = o.obfuscate(p)
+        e = time.time()
+        times.append(e - s)
+        lens.append(len(out.code))
+    return _avg(lens), (_avg(lens) / float(len(p.code))) * 100., _avg(times)
+
+
+def profile_obfuscators(obfs, progs):
+    for o in obfs:
+        for p in progs:
+            print '%s on %s:' % (o, p),
+            s, p, t = profile_obfuscator(o, p)
+            print 'size = %3.3f, pct = %3.2f%%, time = %3.3f' % (s, p, t)
+
+
+profile_combiners = profile_obfuscators
 
 
 def main():
-    jquery = Program('jquery', LANG_JS, JS_JQUERY)
-    fact = Program('fact', LANG_JS, JS_FACT)
+    progs = [
+        Program('jquery', LANG_JS, JS_JQUERY),
+        # Program('fact', LANG_JS, JS_FACT),
+        Program('angular', LANG_JS, JS_ANGULAR),
+        # Program('fib', LANG_JS, JS_FIB),
+        Program('dojo', LANG_JS, JS_DOJO),
+        # Program('popcorn', LANG_JS, JS_POPCORN),
+        Program('swig', LANG_JS, JS_SWIG),
+        Program('chart', LANG_JS, JS_CHART),
+        Program('cookies', LANG_JS, JS_COOKIES),
+        # Program('modernizr', LANG_JS, JS_MODERNIZR),
+        Program('jcarousel', LANG_JS, JS_JCAROUSEL),
+        # Program('hammer', LANG_JS, JS_HAMMER),
+        Program('backbone', LANG_JS, JS_BACKBONE),
+        Program('epoch', LANG_JS, JS_EPOCH),
+        Program('physicsjs', LANG_JS, JS_PHYSICSJS),
+        # Program('soundjs', LANG_JS, JS_SOUNDJS),
+        Program('videojs', LANG_JS, JS_VIDEOJS),
+        Program('raphael', LANG_JS, JS_RAPHAEL),
+        Program('highlight', LANG_JS, JS_HIGHLIGHT),
+        # Program('jqueryui', LANG_JS, JS_JQUERY_UI),
+        # Program('mootools', LANG_JS, JS_MOOTOOLS),
+        # Program('underscore', LANG_JS, JS_UNDERSCORE),
+    ]
 
     o_norm = YUIObfuscator(YUI_PATH)
     o_rand = YUIObfuscator(YUI_PATH, randomize=True)
     o_closure = ClosureObfuscator(CLOSURE_PATH)
 
+    """
     o_leak_in_code = YUIObfuscator(YUI_PATH, leak='in-code')
     o_leak_run_time = YUIObfuscator(YUI_PATH, leak='run-time')
     o_leak_external = YUIObfuscator(YUI_PATH, leak='external')
+    """
 
     c_norm_rand_closure = CascadeCombiner(obfs=[o_norm, o_rand, o_closure])
-    c_norm_rand_closure_in_code = CascadeCombiner(obfs=[o_norm, o_rand,
-                                                        o_closure,
-                                                        o_leak_in_code])
+    c_norm_rand_closure_norm = CascadeCombiner(obfs=[o_norm, o_rand, o_closure,
+                                                     o_norm])
 
     # profile normal obfuscators
-    # profile_obfuscator(o_norm, jquery)
-    # profile_obfuscator(o_rand, jquery)
-    # profile_obfuscator(o_closure, jquery)
+    profile_obfuscators([o_norm, o_rand, o_closure],
+                        progs)
 
     # profile malicious obfuscators
-    # profile_obfuscator(o_leak_in_code, jquery)
-    # profile_obfuscator(o_leak_run_time, jquery)
-    # profile_obfuscator(o_leak_external, jquery)
+    """
+    profile_obfuscators([o_leak_in_code, o_leak_run_time, o_leak_external],
+                        progs)
+    """
 
     # profile cascade combiners
-    # profile_combiner(c_norm_rand_closure, jquery)
-    # profile_combiner(c_norm_rand_closure_in_code, jquery)
+    profile_combiners([c_norm_rand_closure, c_norm_rand_closure_norm], progs)
 
     # profile 3-out-of-4 combiners
     m = JSMajorityChooser()
-    f_norm_rand_closure_run_time = C3OutOf4Combiner(
-        obfs=[o_norm, o_rand, o_closure, o_leak_run_time], maj=m)
-    f_norm_rand_closure_in_code = C3OutOf4Combiner(
-        obfs=[o_norm, o_rand, o_closure, o_leak_in_code], maj=m)
-    # profile_combiner(f_norm_rand_closure_run_time, jquery)
-    # profile_combiner(f_norm_rand_closure_in_code, jquery)
-    profile_combiner(f_norm_rand_closure_in_code, fact)
+    f_norm_rand_closure_norm = C3OutOf4Combiner(
+        obfs=[o_norm, o_rand, o_closure, o_norm], maj=m)
+    f_closure_rand_closure_norm = C3OutOf4Combiner(
+        obfs=[o_closure, o_rand, o_closure, o_norm], maj=m)
+    profile_combiners([f_norm_rand_closure_norm, f_closure_rand_closure_norm],
+                      progs)
 
     return 0
 
--- pyobf/pyobf/consts.py
+++ pyobf/pyobf/consts.py
@@ -5,13 +5,25 @@ _base = os.path.dirname(__file__)
 _js_obf = os.path.join(_base, r'..\..\obfuscators\js')
 _tests = os.path.join(_base, r'tests')
 
-YUI_PATH = os.path.join(_js_obf, r'yuicompressor-2.4.8-leak\build\yuicompressor-2.4.8.jar')
+YUI_PATH = os.path.join(_js_obf, r'yuicompressor-2.4.8-leak\build' +
+                                 '\yuicompressor-2.4.8.jar')
 CLOSURE_PATH = os.path.join(_js_obf, r'closure\compiler.jar')
 
 JS_FIB = os.path.join(_tests, r'fib.js')
 JS_FACT = os.path.join(_tests, r'fact.js')
-JS_JQUERY = os.path.join(_js_obf, r'yuicompressor-2.4.8-leak\tests\jquery-1.6.4.js')
+JS_JQUERY = os.path.join(_tests, r'jquery-1.6.4.js')
 JS_ANGULAR = os.path.join(_tests, r'angular-1.4.5.js')
+JS_DOJO = os.path.join(_tests, r'dojo.js.uncompressed.js')
+JS_HIGHLIGHT = os.path.join(_tests, r'highlight.js')
+JS_RAPHAEL = os.path.join(_tests, r'raphael.js')
+JS_SWIG = os.path.join(_tests, r'swig.js')
+JS_CHART = os.path.join(_tests, r'Chart.js')
+JS_COOKIES = os.path.join(_tests, r'cookies.js')
+JS_JCAROUSEL = os.path.join(_tests, r'jquery.jcarousel.js')
+JS_BACKBONE = os.path.join(_tests, r'backbone.js')
+JS_EPOCH = os.path.join(_tests, r'epoch.js')
+JS_PHYSICSJS = os.path.join(_tests, r'physicsjs.js')
+JS_VIDEOJS = os.path.join(_tests, r'video.js')
 
 
 del _base
